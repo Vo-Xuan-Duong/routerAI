960ff8ccdf6b92ceabd56b717557007741992ec9
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-16 20:48:34 +0700

    Add quota history record model

--- src/core/Quota.hpp
+++ src/core/Quota.hpp
@@ -29,4 +29,21 @@ struct QuotaSnapshot {
     std::vector<QuotaBucket> buckets;
 };
 
+struct QuotaHistoryEntry {
+    std::int64_t snapshotId{0};
+    std::string capturedAt;
+    std::string accountId;
+    std::optional<bool> ordinaryUsageAllowed;
+    std::string providerAccountId;
+    std::string limitId;
+    std::string limitName;
+    std::string model;
+    std::string planType;
+    std::string reachedType;
+    std::string windowName;
+    double usedPercent{0.0};
+    std::optional<std::int64_t> windowDurationMinutes;
+    std::optional<std::int64_t> resetsAtUnix;
+};
+
 }  // namespace routerai
