87b8d05103bcf63f5500810e04b30cdebaf73b3b
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-16 20:18:16 +0700

    Add account login and auth status management

--- src/core/AccountManager.hpp
+++ src/core/AccountManager.hpp
@@ -1,18 +1,35 @@
 #pragma once
 
 #include "core/Account.hpp"
+#include "core/Provider.hpp"
 #include "storage/SQLiteDatabase.hpp"
 
+#include <optional>
 #include <string>
 #include <vector>
 
 namespace routerai {
 
+struct AccountLoginOutcome {
+    Account account;
+    LoginResult result;
+};
+
+struct AccountAuthOutcome {
+    Account account;
+    AuthStatus auth;
+};
+
 class AccountManager {
 public:
     explicit AccountManager(SQLiteDatabase& database);
 
-    Account addCodexPlaceholder();
+    Account addCodexAccount();
+    AccountLoginOutcome loginAccount(const std::string& accountId, bool useBrowser);
+    AccountAuthOutcome refreshAccountStatus(const std::string& accountId);
+    void refreshAllAccountStatuses();
+
+    std::optional<Account> findAccount(const std::string& accountId) const;
     std::vector<Account> listAccounts() const;
     std::size_t countAccounts() const;
 
