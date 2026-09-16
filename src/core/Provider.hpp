7c6cad2f519e3dd68cbf319a9df0c7313372f072
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-16 20:37:07 +0700

    Expose provider account profile reads

--- src/core/Provider.hpp
+++ src/core/Provider.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "core/Account.hpp"
+#include "core/AccountProfile.hpp"
 #include "core/Quota.hpp"
 
 #include <string>
@@ -29,6 +30,7 @@ public:
     virtual Account createPlaceholderAccount(const std::string& accountId) const = 0;
     virtual LoginResult login(Account& account, const LoginOptions& options) const = 0;
     virtual AuthStatus authStatus(const Account& account) const = 0;
+    virtual AccountProfile readProfile(const Account& account) const = 0;
     virtual QuotaSnapshot readQuota(const Account& account) const = 0;
 };
 
