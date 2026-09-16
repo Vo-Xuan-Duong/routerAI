572e2471262d5fef3d6445dd4bcce895f30fb6ce
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-16 23:00:18 +0700

    Add Z.ai account modes and routing support helpers

--- src/providers/zai/ZaiProvider.hpp
+++ src/providers/zai/ZaiProvider.hpp
@@ -10,11 +10,13 @@ class ZaiProvider final : public Provider {
 public:
     std::string name() const override;
     Account createPlaceholderAccount(const std::string& accountId) const override;
+    Account createAccount(const std::string& accountId, const std::string& mode) const;
     LoginResult login(Account& account, const LoginOptions& options) const override;
     AuthStatus authStatus(const Account& account) const override;
     AccountProfile readProfile(const Account& account) const override;
     QuotaSnapshot readQuota(const Account& account) const override;
 
+    static bool supportsUnifiedRouting(const Account& account);
     static std::string codingBaseUrl();
     static std::string generalBaseUrl();
 };
