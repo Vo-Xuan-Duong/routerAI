78c4b2f72fbbf469fd8ca49165d67e4a762337a0
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-16 20:16:50 +0700

    Extend provider abstraction with authentication

--- src/core/Provider.hpp
+++ src/core/Provider.hpp
@@ -6,12 +6,28 @@
 
 namespace routerai {
 
+struct LoginOptions {
+    bool useBrowser{false};
+};
+
+struct LoginResult {
+    bool success{false};
+    std::string detail;
+};
+
+struct AuthStatus {
+    bool authenticated{false};
+    std::string detail;
+};
+
 class Provider {
 public:
     virtual ~Provider() = default;
 
     virtual std::string name() const = 0;
     virtual Account createPlaceholderAccount(const std::string& accountId) const = 0;
+    virtual LoginResult login(Account& account, const LoginOptions& options) const = 0;
+    virtual AuthStatus authStatus(const Account& account) const = 0;
 };
 
 }  // namespace routerai
