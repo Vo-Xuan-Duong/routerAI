6ed9b8b59956ca7bb45f139d305a6aaab8b0a15d
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-17 15:14:29 +0700

    Add Windows run helper

---
+++ run.cmd
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+cd /d "%~dp0"
+
+call build.cmd
+if errorlevel 1 exit /b 1
+
+if not exist build\router.exe (
+  echo [routerAI] build\router.exe was not produced.
+  exit /b 1
+)
+
+set "PATH=%CD%\build\vcpkg_installed\x64-mingw-dynamic\bin;C:\Mingw64\bin;%PATH%"
+build\router.exe
+exit /b %errorlevel%
