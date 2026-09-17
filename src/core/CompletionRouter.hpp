ee5b977c36bed8a1a1aabc2bfb245a7a8887e0e4
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-17 11:29:59 +0700

    Add completion streaming router API

--- src/core/CompletionRouter.hpp
+++ src/core/CompletionRouter.hpp
@@ -4,7 +4,9 @@
 #include "security/CredentialStore.hpp"
 #include "storage/SQLiteDatabase.hpp"
 
+#include <functional>
 #include <string>
+#include <string_view>
 
 namespace routerai {
 
@@ -16,6 +18,18 @@ struct CompletionRouteResult {
     std::string provider;
 };
 
+struct CompletionStreamResult {
+    long statusCode{500};
+    std::string body;
+    std::string contentType{"text/event-stream"};
+    std::string accountId;
+    std::string provider;
+    std::string streamMode{"buffered"};
+    bool emittedData{false};
+};
+
+using CompletionStreamCallback = std::function<bool(std::string_view)>;
+
 class CompletionRouter {
 public:
     CompletionRouter(
@@ -26,6 +40,11 @@ public:
     CompletionRouteResult chatCompletions(
         const std::string& groupId,
         const std::string& requestBody);
+
+    CompletionStreamResult streamChatCompletions(
+        const std::string& groupId,
+        const std::string& requestBody,
+        const CompletionStreamCallback& onChunk);
 
 private:
     SQLiteDatabase& database_;
