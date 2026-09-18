e6ebaa4b1ea1ef1d029363f427a303efccd23ae8
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-18 11:23:15 +0700

    Persist validated account priority

--- src/core/AccountManagerLifecycle.cpp
+++ src/core/AccountManagerLifecycle.cpp
@@ -18,6 +18,21 @@ Account AccountManager::setAccountEnabled(const std::string& accountId, bool ena
     return *account;
 }
 
+Account AccountManager::setAccountPriority(const std::string& accountId, int priority) {
+    if (priority < -100000 || priority > 100000) {
+        throw std::runtime_error("Account priority must be between -100000 and 100000");
+    }
+
+    auto account = database_.findAccount(accountId);
+    if (!account) {
+        throw std::runtime_error("Account not found: " + accountId);
+    }
+
+    account->priority = priority;
+    database_.updateAccount(*account);
+    return *account;
+}
+
 void AccountManager::removeAccount(const std::string& accountId) {
     const auto account = database_.findAccount(accountId);
     if (!account) {
